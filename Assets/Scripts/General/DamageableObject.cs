f779b320dd5e25a9e8cd5c4334b9397f3c48422e
Jeremy Smellie <jeremysmellie@gmail.com>
2014-12-18 21:17:57 -0500

    Created a very basic object that can be damaged.  Will be the base for anything that will have health more or less.

---
+++ Assets/Scripts/General/DamageableObject.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class DamageableObject : MonoBehaviour
+{
+  [SerializeField]
+  protected float _maxHealth;
+
+  [SerializeField]
+  protected float _maxArmour;
+
+  protected float _health;
+  public float health
+  {
+    get
+    {
+      return _health;
+    }
+  }
+
+  protected float _armour;
+  public float armour
+  {
+    get
+    {
+      return _armour;
+    }
+    set
+    {
+      if (value > _maxArmour)
+      {
+        _armour = _maxArmour;
+      }
+      else if (value < 0.0f)
+      {
+        _armour = 0.0f;
+      }
+      else
+      {
+        _armour = value;
+      }
+    }
+  }
+
+  public void DamageTaken(float damage)
+  {
+    damage = damage * (1 - _armour);
+
+    if (damage < 0)
+    {
+      return;
+    }
+
+    _health -= damage;
+
+    if (_health < 0)
+    {
+      //TODO: Dead
+    }
+  }
+
+  public abstract void Dead();
+}
