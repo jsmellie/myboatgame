935b52d653a3e3463ed9b8507879493a74b5d163
Jeremy Smellie <jeremysmellie@gmail.com>
2015-01-14 20:46:32 -0500

    Started the spawner system.
    Making it generalized so that it can be reused in the future for single player or to spawn obstacles.
    
    FourPlayerSpawner.cs will spawn 4 players at specified positions with specified rotation and they render a gizmo to make it easier to see in the editor

---
+++ Assets/Scripts/Spawners/BaseSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class BaseSpawner : MonoBehaviour
+{
+  [System.Serializable]
+  public struct PlayerShipSpawnInfo
+  {
+    #region Fields & Properties
+
+    public Vector2 spawnLocation;
+    public float rotation;
+    public Ship shipPrefab; //TODO: Probably resource load this
+    public Sprite hullImage; //TODO: Probably resource load this
+
+    #endregion
+
+    #region Constructors
+
+    public PlayerShipSpawnInfo(Vector2 spawnLocation, float rotation, Ship shipPrefab, Sprite hullImage)
+    {
+      this.spawnLocation = spawnLocation;
+      this.rotation = rotation;
+      this.shipPrefab = shipPrefab;
+      this.hullImage = hullImage;
+    }
+
+    public Ship Spawn()
+    {
+      Vector3 pos = (Vector3)spawnLocation;
+
+      Ship spawnedShip = GameObject.Instantiate(shipPrefab, pos, Quaternion.Euler(new Vector3(0,0,rotation))) as Ship;
+
+      return spawnedShip;
+    }
+
+    #endregion
+  }
+
+  #region Fields & Properties
+
+  protected bool _isSpawning = false;
+  public virtual bool isSpawning
+  {
+    get { return _isSpawning; }
+  }
+
+  #endregion
+
+  #region Functions
+
+  public abstract bool StartSpawner();
+
+  public abstract bool StopSpawner();
+
+  #endregion
+}
---
+++ Assets/Scripts/Spawners/FourPlayerSpawner.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FourPlayerSpawner : BaseSpawner
+{
+  #region Fields & Properties
+
+  [Header("Player Spawn Info")]
+  [SerializeField]
+  protected PlayerShipSpawnInfo player1SpawnInfo;
+  [SerializeField]
+  protected PlayerShipSpawnInfo player2SpawnInfo;
+  [SerializeField]
+  protected PlayerShipSpawnInfo player3SpawnInfo;
+  [SerializeField]
+  protected PlayerShipSpawnInfo player4SpawnInfo;
+
+#if UNITY_EDITOR
+  [Header("Editor Fields")]
+  [SerializeField]
+  protected Color player1GizmoColor = Color.green;
+  [SerializeField]
+  protected Color player2GizmoColor = Color.red;
+  [SerializeField]
+  protected Color player3GizmoColor = Color.blue;
+  [SerializeField]
+  protected Color player4GizmoColor = Color.yellow;
+#endif
+
+  #endregion
+
+  #region Functions
+
+  public override bool StartSpawner()
+  {
+    //TODO: Spawn all the players here based on the positions specified
+    return true;
+  }
+
+  public override bool StopSpawner()
+  {
+    throw new System.NotImplementedException();
+  }
+
+#if UNITY_EDITOR
+  protected void OnDrawGizmosSelected()
+  {
+    //Draw player1 gizmo
+    DrawPlayerGizmo(player1GizmoColor, player1SpawnInfo.spawnLocation, player1SpawnInfo.rotation);
+    DrawPlayerGizmo(player2GizmoColor, player2SpawnInfo.spawnLocation, player2SpawnInfo.rotation);
+    DrawPlayerGizmo(player3GizmoColor, player3SpawnInfo.spawnLocation, player3SpawnInfo.rotation);
+    DrawPlayerGizmo(player4GizmoColor, player4SpawnInfo.spawnLocation, player4SpawnInfo.rotation);
+  }
+
+  protected void DrawPlayerGizmo(Color gizmoColor, Vector2 position, float rotation)
+  {
+    //Set gizmo color
+    Gizmos.color = gizmoColor;
+
+    //Draw cube for position
+    Gizmos.DrawWireCube((Vector3)position, Vector3.one / 4);
+    
+    //Draw arrow for rotation
+    //TODO : Use Gizmos.DrawLine to make the arrow
+  }
+#endif
+
+
+  #endregion
+}
